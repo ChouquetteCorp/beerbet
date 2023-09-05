0a17c1bd986e3da8ecc579aa04a257503030810b
BilelJegham <contact.bileljegham@gmail.com>
2023-09-05 19:38:48 +0200

    fix: cleanup

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,16 +1,41 @@
 import { createI18n } from 'vue-i18n'
+import en from './locales/en.json'
 import fr from './locales/fr.json'
 
 const i18n = createI18n({
   locale: navigator.language,
-  fallbackLocale: 'fr',
+  fallbackLocale: 'en',
   fallbackWarn: false,
   missingWarn: false,
   legacy: false,
   messages: {
+    en,
     fr,
   },
   datetimeFormats: {
+    en: {
+      long: {
+        weekday: 'short',
+        month: 'numeric',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+      },
+      short: {
+        month: 'numeric',
+        day: 'numeric',
+      },
+      date: {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+      },
+      time: {
+        hour: '2-digit',
+        minute: '2-digit',
+      },
+    },
     fr: {
       long: {
         weekday: 'short',
